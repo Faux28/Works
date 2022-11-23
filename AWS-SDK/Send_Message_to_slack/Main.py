8a01c66f41765c4add974ebb478d7828491027a6
Mohammed Irfan <mdirfan2180@gmail.com>
2022-11-23 12:54:19 +0530

    Update Main.py

--- AWS-SDK/Send_Message_to_slack/Main.py
+++ AWS-SDK/Send_Message_to_slack/Main.py
@@ -1,4 +1,5 @@
 import json
+
 from os import getenv
 
 import requests
@@ -6,9 +7,10 @@ import requests
 def lambda_handler(event, context):
     
     webhook_url = getenv('Webhook_URL')
-    message = '{"text":"Hello"}'
+    message = json.dumps({'text': 'data'})
     response = requests.post(webhook_url,data=message)
     
     return {
         'statusCode': response.status_code,
+	'body': json.dumps(response.text)
     }
