7169bbbfd574a2cfc6b8fe78dedcaddb49945a53
Mohammed Irfan <mdirfan2180@gmail.com>
2022-11-23 09:53:07 +0530

    Delete send_msg_to_slack.py

--- AWS-SDK/send_msg_to_slack.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-message = '{"text":"message sent to slack"}'
-response = requests.post('https://hooks.slack.com/services/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',data=message)
-
-print(response.text)
